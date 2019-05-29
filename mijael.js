70b804dd702c54cf244b1d9e251a2e191b0ea1c1
MijaelPime <mijael.pimentel@gmail.com>
2019-05-28 19:03:26 -0500

    arhivo mijael.js

--- mijael.js
+++ mijael.js
@@ -1,22 +1,33 @@
-let opcion  =   parseInt(prompt("Cual Opción de ejercicio quieres ejecutar 1.- Pizzarra 2.- Peso"));
+let opcionJuego = 0;
+do {
+    opcionJuego = parseInt(prompt("Escoje una Opcion \n 1) Jugar al Regaño \n 2) Jugar Peso \n Cancelar para Salir"));
 
-if(opcion === 1) {
-    let numero1 =   parseInt(prompt("Paco Escribiras los Números Desde: "));
-    let numero2 =   parseInt(prompt("Escribiras los Números Hasta: "));
-    
-    console.log("Los numeros que Paco escribio son: ");
-    for(let contador = numero1; contador <= numero2; contador = contador + 1) {
-        console.log(contador);
-    }
-} else {
-    let miPeso = parseFloat(prompt("¿Cual es tu peso?"));
-    let pesoIdeal = parseFloat(prompt("¿Cual es tu peso ideal?"));
-    let kilosXsemana = parseFloat(prompt("¿Kilos por semana?"));
-
-    let semanas = mipeso/kilosXsemana;
-
-    for(let contador2 = pesoideal; contador >= miPeso; contador2 = contador2+1) {
+    if(opcionJuego === 2) {
+        let pesoActual = parseFloat(prompt("¿Cual es tu peso Actual?"));
+        let pesoIdeal = parseFloat(prompt("¿Cual es tu peso Ideal?"));
+        let pesoXsemana = parseFloat(prompt("¿Cuantos Kilos quieres bajar por semana?"));
         
-    }
+        let semana=1;
+        
+        while(pesoIdeal < pesoActual) {
+        pesoActual = pesoActual - pesoXsemana;
+        semana = semana+1;
+        }
+        console.log(pesoIdeal);
+        console.log(pesoActual);
+        alert("Semas en las que tardaras en pornerte en forma: "+semana);
 
-} 
+    }else if(opcionJuego === 1) {
+        let numero1 = parseInt(prompt("Ingresa el primer Número"));
+        let numero2 = parseInt(prompt("Ingresa el ultimo Número"));
+        let validacion  =  numero1 < numero2 && numero2 > numero1;
+        let cadena = "";
+        if(validacion){
+            for (let contador = 1; numero1 <= numero2; contador = contador + 1) {
+                cadena = cadena + " "+ numero1;
+                numero1 = numero1 + 1;
+            }
+            alert(cadena);
+        }
+    }
+}while(opcionJuego <= 3);
